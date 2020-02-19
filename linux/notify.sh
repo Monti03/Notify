f2e8e23ac779a650e74ae7ba61b40e90eb601e9a
Monti03 <francesco.montano.97@gmail.com>
2020-02-19 11:41:05 +0100

    Update notify.sh

--- linux/notify.sh
+++ linux/notify.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+while getopts d:t:c: option
+do
+case "${option}"
+in
+d) DEST=${OPTARG};;
+t) TITLE=${OPTARG};;
+c) TEXT=${OPTARG};;
+esac
+done
+
+echo $DEST
+echo $TITLE
+echo $TEXT
+
+if [ -z "$DEST" ]
+then
+      echo "use option -d to set the destination id"
+      
+fi
+
 curl -g -s -G \
     'https://us-central1-notify-15448.cloudfunctions.net/sendNotification' \
-    --data-urlencode "to=nad9P73" \
-    --data-urlencode "text=ciao" \
-    --data-urlencode "title=caio" 
+    --data-urlencode "to=$DEST" \
+    --data-urlencode "text=$TEXT" \
+    --data-urlencode "title=$TITLE" 
+
+echo ""
